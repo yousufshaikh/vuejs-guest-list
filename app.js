f05d334b2aa903e70d6906c072011a0a08584b02
yousuf451 <yousufshaikh92@gmail.com>
2018-04-02 19:54:41 +0500

    This is initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+var vm1 = new Vue({
+    el: '#app',
+    data:{
+        event:{
+            monthAndDate: 'March 14th - 16th',
+            guestTitle: 'Summer Festival!',
+            eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!",
+            exclusiveOffers: 'Add your name to the guest list for exclusive offers:'
+        },
+        newNameText: '',
+        guestNameList: [],
+        eventCapacity: 25,
+        eventCapacityPercentage: 0,
+        textColorChange: "",
+        inlineLevel: 'inline'
+    },
+    methods:{
+        formSubmitted: function(){
+            console.log("Method Work");
+            if(this.newNameText.length > 0 && this.guestNameList.length <= 24 ){    
+            this.guestNameList.push(this.newNameText);
+            this.newNameText = '';
+            this.eventCapacityPercentage = this.guestNameList.length / (this.eventCapacity / 100);
+            }
+        },
+        keySpace: function(){
+            alert("You Press Space bar");
+        },
+        keyEsc: function(){
+            alert("You Press ESC button");
+        },
+        colorChange: function(){
+            this.textColorChange = "textColor";
+        }
+    },
+    computed:{
+        sortNames: function(){
+            console.log("Computed Work");
+            return this.guestNameList.sort();
+            }
+    },
+    watch:{
+        guestNameList: function(data){
+            console.log("watch object works");
+        }
+    },
+    filters:{
+        formatName: function(value){
+            console.log(value);
+            return value.slice(0,1).toUpperCase() + value.slice(1).toLowerCase();
+        }
+    }
+})
+
+var vm2 = new Vue({
+    el: '#navigation',
+    data: {
+        webHeading: 'Guest List',
+        navLinks:[{name: "Home", id:1, url: "https://www.google.com"}, {name: "Contact", id:2, url: "https://www.twitter.com"}, {name: "About", id:3, url: "https://www.facebook.com"}]
+    },
+    methods: {
+        capacityChange: function(){
+            vm1.eventCapacity = 15
+        }
+    }
+})
+
+console.log(vm1.$data);
